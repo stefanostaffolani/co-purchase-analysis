922546c0a9113cc5b6aa770e78d8567ca3043c50
stefanostaffolani <staffolani.stefano@gmail.com>
2025-08-17 17:58:32 +0200

    feat: add main for GCP and dataset

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,30 +6,40 @@ import org.apache.spark.sql.SparkSession
 object CoPurchaseAnalysis {
 
   def main(args: Array[String]): Unit = {
-    if (args.length != 2) {
-      println("The arguments must be 2")
+    if (args.length != 3) {
+      println("The arguments must be 3")
       return
     }
     val spark = SparkSession.builder
       .appName("CoPurchase analysis")
-      // .config(conf)
       .getOrCreate()
+    val numCores = 4;
+    val numWorkers = args(2).toInt
+    if (numWorkers > 4 || numWorkers < 1) {
+      println("the number of workers must be between 1 and 4")
+      return
+    }
+    val partitions = 2 * numCores * numWorkers;
+    println(s"Partitions: ${partitions}")
     // since we want to use an RDD we have to call the sparkContext
     // spark.read.text("path/to/file.txt") return a DF
     // https://spark.apache.org/docs/latest/api/java/org/apache/spark/SparkContext.html#defaultParallelism()
+    val startTime = System.nanoTime()
+
     val input = spark.sparkContext
       .textFile(args(0))
       .map((line) => line.split(","))
       .map(w => (w(0).toInt, w(1).toInt))
+
     val partitioned = input.partitionBy(
-      new HashPartitioner(2 * spark.sparkContext.defaultParallelism)
+      new HashPartitioner(partitions)
     )
     try {
-      val result = partitioned
+      val prodPair = partitioned
         .groupByKey()
         .flatMap {
           case (_, products) => {
-            val prodlist = products.toList
+            val prodlist = products.toSeq
             for {
               x <- prodlist
               y <- prodlist
@@ -37,18 +47,21 @@ object CoPurchaseAnalysis {
             } yield ((x, y), 1)
           }
         }
-        .reduceByKey(_ + _)
-        .map { case (prods, numbers) =>
-          s"prod1: ${prods._1}, prod2: ${prods._2} => $numbers"
-        }
-        .coalesce(1)
-        .saveAsTextFile(args(1))
+      val result = prodPair.reduceByKey(_ + _)
+      val prettified = result.map { case (prods, numbers) =>
+        s"${prods._1},${prods._2},$numbers"
+      }
+      val toOne = prettified.repartition(1)
+      toOne.saveAsTextFile(args(1))
+      val endTime = System.nanoTime()
+      val runTime = (endTime - startTime) / 1e9 
+      println(s"T : ${runTime} s")
+
     } catch {
       case e: Throwable => println(e)
     } finally {
       spark.stop()
     }
     // https://stackoverflow.com/questions/31610971/spark-repartition-vs-coalesce
-    // We use coalesce because we want to decrease the total number of partition so it is more efficient
   }
 }
